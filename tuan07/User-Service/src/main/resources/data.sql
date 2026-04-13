569328d25907a0588f6b1504b0ce3aa5b6c797ff
kieutrang47 <kieutrang060811@gmail.com>
2026-04-13 19:14:59 +0700

    mariadb

--- tuan07/User-Service/src/main/resources/data.sql
+++ tuan07/User-Service/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (username, password, role) VALUES ('admin', '123456', 'ADMIN');
-INSERT INTO users (username, password, role) VALUES ('khachhang', '123456', 'USER');
+INSERT IGNORE INTO users (username, password, role) VALUES ('admin', '123456', 'ADMIN');
+INSERT IGNORE INTO users (username, password, role) VALUES ('khachhang', '123456', 'USER');
